2efd934da2b8d48f6465ed0d453aa2105f078f2b
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2020-11-16 22:51:35 +0100

    IRepos with GetAll and GetById

---
+++ Kwitter/Data/ICommentRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kwitter.Data
+{
+    public interface ICommentRepo
+    {
+        IEnumerable<Comment> GetAllComments();
+        Comment GetCommentById();
+    }
+}
