fd8a645e7e889eab04e11758447ac4f28b03af0b
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2020-11-16 22:59:49 +0100

    SqlRepos with no implementation (need db context)

---
+++ Kwitter/Data/SqlKweetRepo.cs
@@ -0,0 +1,21 @@
+﻿using Kwitter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kwitter.Data
+{
+    public class SqlKweetRepo : IKweetRepo
+    {
+        public IEnumerable<Kweet> GetAllKweets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Kweet GetKweetById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
