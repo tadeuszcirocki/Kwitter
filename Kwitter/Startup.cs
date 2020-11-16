1b716e466fce1702e81de1184a3ed34c689d764f
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2020-11-16 23:25:01 +0100

    setup before adding methods to controllers

--- Kwitter/Startup.cs
+++ Kwitter/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kwitter.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,10 @@ namespace Kwitter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<ICommentRepo, SqlCommentRepo>();
+            services.AddScoped<IKweetRepo, SqlKweetRepo>();
+            services.AddScoped<IUserRepo, SqlUserRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
