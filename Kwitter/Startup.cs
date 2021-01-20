ed84e69b05e6330ac16523a8921d2168520d38f3
Tadeusz Cirocki <tadeusz.cirocki1@gmail.com>
2021-01-20 12:33:38 +0100

    enable cors for all

--- Kwitter/Startup.cs
+++ Kwitter/Startup.cs
@@ -28,6 +28,14 @@ namespace Kwitter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
             services.AddControllers().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
@@ -51,6 +59,8 @@ namespace Kwitter
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
